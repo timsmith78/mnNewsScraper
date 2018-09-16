7b5f110b3c95b81f583aebe90045b29eb53f87df
Timothy Smith <timsmith78@yahoo.com>
2018-09-15 23:30:55 -0500

    Adding heroku logic

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 // Setup database
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/mnNewsScraper')
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mnNewsScraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 const db = require('./models')
 
 // Setup web server
